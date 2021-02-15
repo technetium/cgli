0e68e62164a7ac5fd036d806fa612b1cc937925f
technetium <github@technetium.be>
2021-02-15 14:39:51 +0100

    Upload actual code

---
+++ cgli/__init__.py
@@ -0,0 +1,1 @@
+from cgli.cgli import *
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cgli",
-    version="0.0.2",
+    version="0.0.3",
     author="Toni Cornelissen",
     author_email="cgli@technetium.be",
     description="utility to execute a pythonscript from both commandline and webserver",
